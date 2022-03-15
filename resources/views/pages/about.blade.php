98831c495e93d0538a5ec8c5ca92b0e5c8cd0d2f
Lamine <lsene31@gmail.com>
2022-03-15 13:01:19 +0000

    Add route names and use them in views

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -5,5 +5,5 @@
 @section('content')
     <p>Built with &hearts; by Technical Boy.</p>
 
-    <p><a href="/">Revenir a la page d'accueil</a></p>
+    <p><a href="{{ route('home') }}">Revenir a la page d'accueil</a></p>
 @endsection
