a97bc58e5571f67208622bf46f4fc9eb9e18b045
Lamine <lsene31@gmail.com>
2022-03-15 10:22:25 +0000

    Move static pages to pages folder and remove link from about page

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -14,7 +14,7 @@
     <p><a href="/">Revenir a la page d'accueil</a></p>
 
     <footer>
-        <p>&copy; Copyright {{ date('Y') }} &middot; <a href="/about-us">About Us</a></p>
+        <p>&copy; Copyright {{ date('Y') }}</p>
     </footer>
 </body>
 
