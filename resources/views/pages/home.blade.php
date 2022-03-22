f48b5eea4775049254f3987d33617fd87b121248
Lamine <lsene31@gmail.com>
2022-03-22 15:18:46 +0000

    Add styling with Tailwind CSS

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -1,9 +1,9 @@
 @extends('app')
 
 @section('content')
-    <img src="{{ asset('images/senegal-flag.png') }}" alt="Senegal Flag">
+    <img src="{{ asset('images/senegal-flag.png') }}" alt="Senegal Flag" class="rounded shadow-md h-32">
 
-    <h1>Hello from Thies!</h1>
+    <h1 class="mt-5 text-3xl sm:text-5xl font-semibold text-indigo-600">Hello from Thies!</h1>
 
-    <p>It's currently {{ date('h:i A') }}.</p>
+    <p class="text-lg text-gray-800">It's currently {{ date('h:i A') }}.</p>
 @endsection
